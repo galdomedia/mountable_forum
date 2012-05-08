35c3a7e43b73236fda5b68adde1ceaca474a5c9d
Bonias <piotr@galdomedia.pl>
2012-05-08 12:18:21 +0200

    precompile simple_forum/admin.css

--- lib/simple_forum/engine.rb
+++ lib/simple_forum/engine.rb
@@ -3,6 +3,7 @@ module SimpleForum
     isolate_namespace SimpleForum
 
     initializer "simple_forum.assets_precompile" do |app|
+      app.config.assets.precompile += ['simple_forum/admin.css']
       app.config.assets.precompile += ['simple_forum/markitup.css', 'simple_forum/markitup.js']
       app.config.assets.precompile += ['simple_forum/moderators.js']
     end
